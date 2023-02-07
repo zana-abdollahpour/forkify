17dc624952a09b3f9d224a337590a18bc1286c1c
zana-abdollahpour <zana.ap4@gmail.com>
2023-02-07 11:52:09 +0330

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5da2b360-a53b-40f5-8a7b-b7e1ab36a14d';
+export const MODAL_CLOSE_SEC = 2.5;
