fa80433ccb534633c52a853ffe52f8e221204e9d
zana-abdollahpour <zana.ap4@gmail.com>
2024-08-21 19:08:37 +0330

    convert decimal to fraction in recipe ingredients

--- src/views/recepieView.ts
+++ src/views/recepieView.ts
@@ -1,3 +1,5 @@
+import Fraction from "fraction.js";
+
 import { Recipe } from "@/types/recipe.types";
 
 class RecipeView {
@@ -89,7 +91,11 @@ class RecipeView {
                 <svg class="recipe__icon">
                   <use href="icons.svg#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity}</div>
+                <div class="recipe__quantity">${
+                  ing.quantity
+                    ? new Fraction(ing.quantity).toFraction(true)
+                    : ""
+                }</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
                   ${ing.description}
